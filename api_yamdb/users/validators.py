8dcd7b75f7b2de7cd380d8d9a3c7423f22fc6efd
Aleksandr Tekutev <tekutiev.sasha@gmail.com>
2023-08-24 19:45:26 +0100

    Fixed superuser creation problem

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -2,5 +2,4 @@ from django.core.validators import RegexValidator
 
 
 class UsernameValidator(RegexValidator):
-    regex = r'^[\w.@+-]+\z',
-    message = 'Имя содержит недопустимые символы'
+    regex = r'^[\w.@+-]+$'
