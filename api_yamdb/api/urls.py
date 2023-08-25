5c9cf3ce21bc608a1d266c16c2436966cb94921c
Ivan Bogdanov <leo09.99.ib@gmail.com>
2023-08-26 00:38:20 +0300

    added rating, one review for title, permissions fix

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -20,7 +20,7 @@ router_v1.register(r'categories', CategoriesViewSet)
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet,
-    basename='reviews'
+    basename='reviews',
 )
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
