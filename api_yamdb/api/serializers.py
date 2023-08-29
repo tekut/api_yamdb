b9a16460bb13f093bdc7c04f8f3eae9954fa78a7
Ivan Bogdanov <leo09.99.ib@gmail.com>
2023-08-29 18:43:43 +0300

    fixes after review

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 
+from django.db.models import Avg
 from rest_framework import serializers
 
 from reviews.models import Category, Comment, Genres, Review, Title
@@ -32,15 +33,8 @@ class TitlesSerializer(serializers.ModelSerializer):
         model = Title
 
     def get_rating(self, obj):
-        title = Title.objects.get(id=obj.id)
-        reviews = title.reviews.all()
-        rating = 0
-        if reviews:
-            for review in reviews:
-                rating += review.score
-            average_value = Decimal(rating / len(reviews))
-            return average_value.quantize(Decimal("1.00"))
-        return None
+        average_rating = Title.objects.annotate(Avg('reviews__score'))
+        return average_rating[0].reviews__score__avg
 
 
 class TitlesPostSerializer(serializers.ModelSerializer):
@@ -124,7 +118,7 @@ class ReviewSerializer(serializers.ModelSerializer):
         if request.method == 'POST':
             author = request.user
             title_id = self.context.get('view').kwargs.get('title_id')
-            if author.reviews.filter(title=title_id):
+            if author.reviews.filter(title=title_id).exists():
                 raise serializers.ValidationError(
                     'Вы уже оставили отзыв к этому произведению.'
                 )
